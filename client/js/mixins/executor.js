85dcb52c59ca71d767834c356c3b4409f64a49bb
Sandile Keswa <me@sandile.io>
2015-02-17 16:29:53 -0500

    changed the executable mixin

--- client/js/mixins/executor.js
+++ client/js/mixins/executor.js
@@ -1,5 +1,11 @@
 var ExecutorMixin = {
-    execute: function() {
+    // Generates preloads a function call with certain parameters.
+    // First argument is the function to call, and what
+    // follows are the arguments thereof.
+    //
+    // Example: to make a console.log that prints "Hello World", simply call:
+    //          this.createExecutable(console.log, 'Hello', 'World');
+    createExecutable: function() {
         var _this = this,
             args = Array.prototype.slice.call(arguments);
         // Isolate the function call
@@ -8,7 +14,8 @@ var ExecutorMixin = {
         args.splice(0, 1);
         // Return the executable
         return function() {
-            fn.apply(_this, args);
+            var innerArgs = Array.prototype.slice.call(arguments);
+            fn.apply(_this, args.concat(innerArgs));
         };
     }
 };
